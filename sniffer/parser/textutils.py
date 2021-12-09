3c4f49d22b260ac34e679619afe8f577c490dd4d
reloadedd <reloadedd@protonmail.com>
2021-12-09 21:41:50 +0200

    Parse commit hash automagically

--- sniffer/parser/textutils.py
+++ sniffer/parser/textutils.py
@@ -1,12 +1,18 @@
 from .. import __version__
+import subprocess
 
 version = __version__.__version__
+commit_hash = subprocess.run(
+    ['git', 'rev-parse', '--short', 'HEAD'],
+    capture_output=True).stdout.decode('utf-8').strip()
+commit_hash = f'[bold cyan]{commit_hash}[/bold cyan]'
+
 BANNER = f"""\
                      ██████╗ ██╗  ██╗███████╗
                     ██╔════╝ ██║  ██║██╔════╝   [Author: Roșca Ionuț]
                     ██║  ███╗███████║███████╗   [License: GPL-3.0 License]
                     ██║   ██║██╔══██║╚════██║   [Version: {version}]
-                    ╚██████╔╝██║  ██║███████║   [Commit: b71d9d8]
+                    ╚██████╔╝██║  ██║███████║   [Commit: {commit_hash}]
                      ╚═════╝ ╚═╝  ╚═╝╚══════╝
                   ＧＵＩ ＨＴＴＰ ＳＮＩＦＦＥＲ
 """
