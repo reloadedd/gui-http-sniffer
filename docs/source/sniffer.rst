2b119cf6bdc9c67a572230b40114723ac45240d1
reloadedd <reloadedd@protonmail.com>
2021-12-22 00:50:05 +0200

    Fix docstrings and update Sphinx files

--- docs/source/sniffer.rst
+++ docs/source/sniffer.rst
@@ -7,8 +7,10 @@ Subpackages
 .. toctree::
    :maxdepth: 4
 
+   sniffer.exceptions
    sniffer.network
    sniffer.parser
+   sniffer.utils
 
 Module contents
 ---------------
--- sniffer/utils/funcutils.py
+++ sniffer/utils/funcutils.py
@@ -4,7 +4,14 @@ from rich.panel import Panel
 from . import constants
 
 
-def handle_error(message):
+def handle_error(message: str) -> None:
+    """Display a visible error message and exit the program.
+
+    Parameters
+    ----------
+    message : str
+        The message to be displayed
+    """
     rich.print(Panel(f'{message}.',
                      title="[red]ERROR[/red]",
                      title_align="left",
